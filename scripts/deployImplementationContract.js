604c084ceb23869625c7f1b37d300a4d0347e9b7
Tayyaba-Pixelette <tayyaba@pixelettetech.com>
2025-07-18 21:49:21 +0500

    added-blacklisting-logic (#5)

--- scripts/deployImplementationContract.js
+++ scripts/deployImplementationContract.js
@@ -3,12 +3,10 @@ const hre = require("hardhat");
 async function main() {
     const fusioToken =  await hre.ethers.getContractFactory("contracts/FusioToken.sol:FusioToken");
     console.log("Deployment started");
-    const FusioToken =  await upgrades.deployProxy(fusioToken, ["FUSIO", "$FUSIO", "0x07d3bdA43236b6A6C8079d49dd2c8839Ec4a811F", "0x07d3bdA43236b6A6C8079d49dd2c8839Ec4a811F"], {
-        initializer: "initialize",
-    });
+    const FusioToken =  await fusioToken.deploy();
     await FusioToken.waitForDeployment();
     const deployedAddress = await FusioToken.getAddress();
-    console.log("Fusio token contract deployed to", deployedAddress);
+    console.log("Implementation Contract deployed to", deployedAddress);
 }
 
 main().catch((error) => {
